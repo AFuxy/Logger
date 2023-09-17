82118c5962af3da187a366ae8da85a8780ed335e
AFuxy <zakygames701@gmail.com>
2023-09-17 02:46:37 +0100

    [+] Tracking members leaving

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require("discord.js");
+const { logger } = require("../index");
+
+client.on("guildMemberRemove", async (member) => {
+    file = null;
+    extra = null;
+    const newInvites = await member.guild.invites.fetch()
+    const oldInvites = invites.get(member.guild.id);
+    const invite = newInvites.find(i => i.uses > oldInvites.get(i.code));
+    const inviter = await client.users.fetch(invite.inviter.id);
+
+    const embed = new EmbedBuilder()
+    .setTitle(" > User Left")
+    .addFields(
+        { name: 'Username', value: `\`\`\`elm\n${member.user.username}\`\`\``},
+        { name: 'ID', value: `\`\`\`elm\n${member.user.id}\`\`\``},
+        // { name: 'Presence', value: `\`\`\`elm\n${member.presence.status}\`\`\``},
+        { name: 'Joined At', value: `<t:${Math.round(member.joinedTimestamp / 1000)}:F>`},
+        { name: 'Invite Code', value: `\`\`\`elm\n${inviter ? invite.code : "None"}\`\`\``}
+    )
+    .setThumbnail(member.user.avatarURL());
+    logger(embed, config.TagID, extra, file);
+})
