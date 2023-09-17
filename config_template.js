558daf312215461b8511b6b1664cfa3b10516ae9
AFuxy <zakygames701@gmail.com>
2023-09-17 03:42:03 +0100

    [~] added version for config for later changes

--- config_template.js
+++ config_template.js
@@ -1,4 +1,5 @@
 module.exports = {
+    V: "0.0.1", //NO NOT TOUCH, THE BOT WILL NOT START!!!
     token: "", // the token of the bot
     presence: "Watching the fish swim.", // a silly presence
     webHookURL: "", // the URL of the webhook to send logs
