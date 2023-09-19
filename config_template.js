e66812e982432527ce5937ff591c3e99ff5ec451
AFuxy <zakygames701@gmail.com>
2023-09-19 01:03:26 +0100

    [~] updated to add new stuffs

--- config_template.js
+++ config_template.js
@@ -1,7 +1,9 @@
 module.exports = {
-    V: "0.0.1", //NO NOT TOUCH, THE BOT WILL NOT START!!!
+    V: "0.0.2", //NO NOT TOUCH, THE BOT WILL NOT START!!!
     token: "", // the token of the bot
     presence: "Watching the fish swim.", // a silly presence
     webHookURL: "", // the URL of the webhook to send logs
     TagID: "", // the ID of the user you'd like to log, leave blank to log EVERYTHING!!
+    webHookFULLURL: "", // this does not need to be used to use the bot
+    TagGuildID: "" // also doesn't need to be used
 }
