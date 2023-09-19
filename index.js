16d0afb6c60f173a1a6126f4fbe204e55d92c10c
AFuxy <zakygames701@gmail.com>
2023-09-19 01:40:30 +0100

    [~] version bump

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if(config.webHookFULLURL){
 async function logger(embed, userID, extra, file = null) {
     embed.setColor("#e36464");
     embed.setTimestamp();
-    embed.setFooter({ text: 'Logger • v0.0.4-Alpha by Bedlam Group', iconURL: 'https://yt3.googleusercontent.com/tDyrkpVDd08Bc67PaUwci855_yiIHv6arCEie-mVdYieBQRkj2_mIhMdiGrSZ6D3PBZfoHso=s176-c-k-c0x00ffffff-no-rj' });
+    embed.setFooter({ text: 'Logger • v0.0.5-Alpha by Bedlam Group', iconURL: 'https://yt3.googleusercontent.com/tDyrkpVDd08Bc67PaUwci855_yiIHv6arCEie-mVdYieBQRkj2_mIhMdiGrSZ6D3PBZfoHso=s176-c-k-c0x00ffffff-no-rj' });
 
     if(userID !== config.TagID) return;
 
