530c8a4958acfa4d5a2ea93ee7202895d17cb3d1
AFuxy <zakygames701@gmail.com>
2023-09-23 01:40:03 +0100

    [+] working command handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, WebhookClient, ActionRowBuilder, ButtonBuilder, Modal, TextInputBuilder, OAuth2Scopes, Partials, resolveColor, Client, Collection, GatewayIntentBits, SelectMenuBuilder, ActivityType, PermissionsBitField, AttachmentBuilder } = require("discord.js");
+const { EmbedBuilder, WebhookClient, ActionRowBuilder, ButtonBuilder, Events, Modal, TextInputBuilder, OAuth2Scopes, Partials, resolveColor, Client, Collection, GatewayIntentBits, SelectMenuBuilder, ActivityType, PermissionsBitField, AttachmentBuilder } = require("discord.js");
 const { exit } = require("process");
 const client = global.client = new Client({ fetchAllMembers: true, intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.MessageContent], scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands], partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.User, Partials.GuildMember, Partials.ThreadMember, Partials.GuildScheduledEvent], ws: { version: "10" } });
 const config = global.config = require("./config");
@@ -7,7 +7,7 @@ const wait = require("timers/promises").setTimeout;
 
 // START | To be moved!
 
-if(config.V !== "0.0.2"){
+if(config.V !== "0.0.3"){
   console.error("Config version does not match, please update your config.js file");
   exit();
 }
@@ -23,10 +23,19 @@ if(config.TagID == null || config.TagID == ""){
   console.error("The TagID cannot be left as null, please update your config.js file");
   exit();
 }
+if(config.clientId == null || config.clientId == ""){
+  console.error("The clientId cannot be left as null, please update your config.js file");
+  exit();
+}
+if(config.guildId == null || config.guildId == ""){
+  console.error("Currently the guildId cannot be left as null, please update your config.js file");
+  exit();
+}
 
 // END
 
 const invites = global.invites = new Collection();
+client.commands = new Collection();
 
 const webhookClient = new WebhookClient({ url: config.webHookURL });
 
@@ -57,21 +66,67 @@ async function fulllogs(guildID, username, icon, message, file = null) {
   webhookClientFULL.send({
     username: username,
     avatarURL: icon,
-    content: message,
+    content: message ? message : "Probably a system message.",
     files: file ? file : []
   })
 }
 
 module.exports = { logger, fulllogs };
 
-var normalizedPath = require("path").join(__dirname, "events");
+var normalizedPathEvents = require("path").join(__dirname, "events");
+var foldersPath = require("path").join(__dirname, "commands");
 
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
+require("fs").readdirSync(normalizedPathEvents).forEach(function(file) {
   require("./events/" + file);
-  console.log(file);
+  console.log("Events: " + file);
 });
 
+// require("fs").readdirSync(normalizedPathCommands).forEach(function(file) {
+//   require("./commands/" + file);
+//   console.log("Commands: " + file);
+// });
+
+const commandFolders = require("fs").readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = require("path").join(foldersPath, folder);
+	const commandFiles = require("fs").readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = require("path").join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+      console.log("Commands: " + file);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
 // //
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 client.on("ready", async () => {
   console.log(`bot online | ${client.user.tag} | Bedlam Logger`);
