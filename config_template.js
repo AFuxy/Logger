3b57d17904d67530097cd46beb11dd8a3065c122
AFuxy <zakygames701@gmail.com>
2023-11-04 23:19:15 +0000

    [-] removed old TagID

--- config_template.js
+++ config_template.js
@@ -1,9 +1,8 @@
 module.exports = {
-    V: "0.0.3", //NO NOT TOUCH, THE BOT WILL NOT START!!!
+    V: "0.0.4", //NO NOT TOUCH, THE BOT WILL NOT START!!!
     token: "", // the token of the bot
     presence: "Watching the fish swim.", // a silly presence
     webHookURL: "", // the URL of the webhook to send logs
-    TagID: "", // the ID of the user you'd like to log, leave blank to log EVERYTHING!!
     webHookFULLURL: "", // this does not need to be used to use the bot
     TagGuildID: "", // also doesn't need to be used
     clientId: "", // the ID of the bot
